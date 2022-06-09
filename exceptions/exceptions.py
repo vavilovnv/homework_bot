0a5cb82a2f2cead34aa9a6e51fe66ede3316548b
Nikolay <53932721+vavilovnv@users.noreply.github.com>
2022-06-09 11:45:36 +0300

    processed comments code review

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,2 +1,6 @@
 class WrongConnectionError(Exception):
     pass
+
+
+class BotSendMessageError(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -9,10 +9,9 @@ from http import HTTPStatus
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
-from exceptions.exceptions import WrongConnectionError
+from exceptions.exceptions import WrongConnectionError, BotSendMessageError
 
 load_dotenv()
-
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -24,28 +23,27 @@ ERROR_MESSAGE_LENGTH = 100
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-HOMEWORK_STATUSES = {
+VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(handler)
-formatter = logging.Formatter(
-    '%(asctime)s [%(levelname)s] %(message)s'
-)
-handler.setFormatter(formatter)
 
 
 def send_message(bot, message):
     """Отправка сообщения telegram-ботом и логирование статуса отправки."""
-    if bot.send_message(TELEGRAM_CHAT_ID, message):
-        logger.info(f'Бот отправил сообщение: {message}')
+    logger.info('Начало отправки сообщения ботом')
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except BotSendMessageError as error:
+        message = (f'Боту не удалось отправить сообщение: {message} по '
+                   f'причине {error}')
+        logger.error(message)
+        raise BotSendMessageError(message)
     else:
-        logger.error(f'Боту не удалось отправить сообщение: {message}')
+        logger.info(f'Бот отправил сообщение: {message}')
 
 
 def get_api_answer(current_timestamp):
@@ -53,31 +51,41 @@ def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     try:
+        logger.info('Начато выполнение запроса к API')
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    except requests.ConnectionError as error:
+    # Переделал except согласно замечанию. Не уверен, что понял его верно.
+    # Если реализация неправильная, прошу уточнить, я доделаю.
+    except Exception as error:
         message = f'Недоступен endpoint сервиса {error}'
-        raise requests.ConnectionError(message)
+        raise ConnectionError(message)
     else:
         if response.status_code == HTTPStatus.OK:
+            logger.info('Запрос к API завершен успешно')
             return response.json()
         else:
-            raise WrongConnectionError(
-                f'Сервер вернул статус {response.status_code}'
+            # Здесь моя попытка обработать ситуации когда запрос в целом
+            # выполнен успешно, но вернул статус отличный от 200, например
+            # 403. Не уверен, что так правильно и что вообще эта проверка
+            # тут нужна и я не переусложнил.
+            message = (
+                f'Неверный результат запроса к API: '
+                f'Сервер вернул статус {response.status_code} '
+                f'при исполнении запроса к url {response.url}. '
+                f'Перенаправление: {["Нет", "Да"][response.is_redirect]}'
             )
+            logger.error(message)
+            raise WrongConnectionError(message)
 
 
 def check_response(response):
     """Проверка структуры данных полученных от сервиса."""
-    keys, data = ['current_date', 'homeworks'], []
-    for key in keys:
-        try:
-            data = response[key]
-        except KeyError:
-            raise KeyError(f'В ответе сервиса нет данных с ключом {key}')
-        else:
-            if key == 'homeworks' and type(data) != list:
-                raise TypeError('Данные homeworks не соответствуют типу list')
-    return data
+    if not isinstance(response, dict):
+        raise TypeError('Ответ сервиса не является словарем')
+    if not all(['current_date' in response, 'homeworks' in response]):
+        raise KeyError('В ответе сервиса нет данных по нужным ключам')
+    if not isinstance(response['homeworks'], list):
+        raise TypeError('Данные homeworks не являются списком')
+    return response['homeworks']
 
 
 def parse_status(homework):
@@ -85,7 +93,7 @@ def parse_status(homework):
     homework_name = homework['homework_name']
     homework_status = homework['status']
     try:
-        verdict = HOMEWORK_STATUSES[homework_status]
+        verdict = VERDICTS[homework_status]
         return f'Изменился статус проверки работы "{homework_name}". {verdict}'
     except KeyError as error:
         raise KeyError(
@@ -104,7 +112,7 @@ def main():
     if not all_tokens_is_ok:
         message = 'Не заполнены переменные окружения.'
         logger.critical(message)
-        raise SystemExit(message)
+        raise sys.exit(message)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     previous_time = datetime.now() - timedelta(days=COUNT_PREVIOUS_DAYS)
@@ -133,4 +141,13 @@ def main():
 
 
 if __name__ == '__main__':
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
+    formatter = logging.Formatter(
+        ('%(asctime)s - %(filename)s, %(funcName)s: %(lineno)d - '
+         '[%(levelname)s] %(message)s')
+    )
+    handler.setFormatter(formatter)
+
     main()
